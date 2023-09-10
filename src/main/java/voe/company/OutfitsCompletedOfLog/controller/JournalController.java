7b3c21bd97ba18ae85f76ad1026d0e82825021b5
Maksim <mysik.bazyk@gmail.com>
2023-09-10 12:20:42 +0300

    refactoring class

--- src/main/java/voe/company/OutfitsCompletedOfLog/controller/JournalController.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/controller/JournalController.java
@@ -2,6 +2,10 @@ package voe.company.OutfitsCompletedOfLog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,16 +24,17 @@ import java.util.Optional;
 @RequestMapping("/journal")
 public class JournalController {
     @Autowired
-    private MagazineService journalService;
+    private MagazineService magazineService;
 
     @GetMapping("/edit")
     public String edit(Model model, @RequestParam(defaultValue = "0") String id) {
-        Optional<JournalEntity> journalEntity = journalService.findById(Long.parseLong(id));
+        Optional<JournalEntity> journalEntity = magazineService.findById(Long.parseLong(id));
         ArrayList<JournalEntity> journalEntities = new ArrayList<>();
         journalEntity.ifPresent(journalEntities::add);
         model.addAttribute("journal", journalEntities);
         return "update";
     }
+
 
     @PostMapping(value = "/edit")
     private String updateEntry(@RequestParam(defaultValue = "0") Long id,
@@ -40,37 +45,40 @@ public class JournalController {
                                @RequestParam String description,
                                @RequestParam String performer,
                                Model model) {
-        JournalEntity entityById = journalService.findById(id).orElseThrow();
+        JournalEntity entityById = magazineService.findById(id).orElseThrow();
         entityById.setNumberOutfit(numberName);
         entityById.setDate(date);
         entityById.setTypeEac(type_etc);
         entityById.setDispatcherNameEts(number_etc);
         entityById.setJobDescription(description);
         entityById.setPerformer(performer);
-        journalService.save(entityById);
+        magazineService.save(entityById);
         return "redirect:/journal/get";
     }
+
 
     @GetMapping("/delete")
     public String delete() {
         return "delete";
     }
 
+
     @GetMapping("/deleteId")
     private String deleteById(@RequestParam(defaultValue = "0") String id_del) {
-        journalService.deleteJournalById(Long.parseLong(id_del));
+        magazineService.deleteJournalById(Long.parseLong(id_del));
         return "journal";
     }
 
+
     @GetMapping("/deleteAll")
     private ResponseEntity<String> deleteAll() {
-        journalService.deleteAll();
+        magazineService.deleteAll();
         return ResponseEntity.ok().body("delete all with database");
     }
 
     @GetMapping("/get")
     private String journal(Model model) {
-        List<JournalEntity> entities = journalService.findAll();
+        List<JournalEntity> entities = magazineService.getFindAll();
         model.addAttribute("entity", entities);
         return "journal";
     }
@@ -84,23 +92,21 @@ public class JournalController {
                               @RequestParam String performer,
                               Model model) {
         Exception checkOut = new CheckOut().check(
-                numberName,
-                date,
-                type_etc,
-                number_etc,
-                description,
-                performer);
+                numberName, date, type_etc,
+                number_etc, description, performer);
         JournalEntity entity = new JournalEntity(
                 numberName, date, type_etc,
                 number_etc, description, performer);
         model.addAttribute("check", checkOut);
-        journalService.addNewEntry(entity);
+        magazineService.addNewEntry(entity);
         return "journal";
     }
 
+
     @GetMapping("/get/by")
+    @PreAuthorize("hasRole('USER')")
     public String dataSearchBy(@RequestParam(defaultValue = "0") String etc, Model model) {
-        model.addAttribute("list", journalService.findBy(etc));
+        model.addAttribute("list", magazineService.findBy(etc));
         return "formSearch";
     }
 
