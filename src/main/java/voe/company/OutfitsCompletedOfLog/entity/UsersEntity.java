859d675989b0023065e10684b8d3d0eb64e737d4
Maksim <mysik.bazyk@gmail.com>
2023-09-10 20:43:03 +0300

    create new class

--- src/main/java/voe/company/OutfitsCompletedOfLog/entity/UsersEntity.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/entity/UsersEntity.java
@@ -5,22 +5,24 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "users")
+@Table(name = "user_details")
 public class UsersEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
     @Column(name = "email")
     private String email;
     @Column(name = "password")
     private String password;
     @Column(name = "role")
     private String role;
+
+    public UsersEntity(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 
     public UsersEntity(){}
 
