d55a6409800f2ce4fca12699f3edcdac16c4d10c
Maksim <mysik.bazyk@gmail.com>
2023-09-10 20:48:24 +0300

    create new classes

--- src/main/java/voe/company/OutfitsCompletedOfLog/service/UsersRoleService.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/service/UsersRoleService.java
@@ -1,7 +1,7 @@
 package voe.company.OutfitsCompletedOfLog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import voe.company.OutfitsCompletedOfLog.entity.UsersEntity;
 import voe.company.OutfitsCompletedOfLog.repository.UserRepository;
@@ -12,29 +12,24 @@ public class UsersRoleService {
     @Autowired
     private UserRepository userRepository;
 
-    public UsersEntity createNewUser(UsersEntity usersEntity){
-        return userRepository.save(usersEntity);
+    public UsersEntity createNewUser(UsersEntity usersEntity) {
+        UsersEntity entity = new UsersEntity(
+                usersEntity.getEmail(),
+                passwordEncoder(usersEntity.getPassword()),
+                usersEntity.getRole());
+        return userRepository.save(entity);
     }
 
-//    public String passwordEncoder(String pass){
-//        return new BCryptPasswordEncoder().encode(pass);
-//    }
-
-    public Integer idRole(String role){
-        if (role.equals("ADMIN")){
-            return 1;
-        } else if (role.equals("USER")) {
-            return 2;
-        }
-        return null;
+    private String passwordEncoder(String pass) {
+        return new BCryptPasswordEncoder().encode(pass);
     }
 
-    public String checkUser(UsersEntity users){
-        for (UsersEntity usersEntity : userRepository.findAll()){
-            if (usersEntity.getEmail().equals(users.getEmail())){
+    public String checkUser(UsersEntity users) {
+        for (UsersEntity usersEntity : userRepository.findAll()) {
+            if (usersEntity.getEmail().equals(users.getEmail())) {
                 return new RuntimeException("Such email exists").getMessage();
             }
         }
-        return userRepository.save(users).getEmail() + "create";
+        return users.getEmail() + "create";
     }
 }
