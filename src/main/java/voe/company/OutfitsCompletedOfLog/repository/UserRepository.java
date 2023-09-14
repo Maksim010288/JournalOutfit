d2d915116c5f969fd1858ce3bea24b35dd6a8d9a
Maksim <mysik.bazyk@gmail.com>
2023-09-14 23:12:33 +0300

    delete methods findByUsername

--- src/main/java/voe/company/OutfitsCompletedOfLog/repository/UserRepository.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/repository/UserRepository.java
@@ -7,7 +7,4 @@ import voe.company.OutfitsCompletedOfLog.entity.UsersEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UsersEntity, Long> {
-
-    @Query(value = "select l from userDet l where l.last_name = ?", nativeQuery = true)
-    UsersEntity findByName(String userName);
 }
