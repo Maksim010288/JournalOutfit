5110c67f8e18daa0bf5f1b6a203755f34024d542
Maksim <mysik.bazyk@gmail.com>
2023-09-14 23:07:35 +0300

    renamed classes

--- src/main/java/voe/company/OutfitsCompletedOfLog/security/WebSecurityConfigurerInMemory.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/security/WebSecurityConfigurerInMemory.java
@@ -1,12 +1,10 @@
 package voe.company.OutfitsCompletedOfLog.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,27 +13,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import voe.company.OutfitsCompletedOfLog.repository.UserRepository;
-
-import javax.sql.DataSource;
+import voe.company.OutfitsCompletedOfLog.service.UserDetService;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfigurer {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Bean
-    public UserDetailsService usersDB() {
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
+public class WebSecurityConfigurerInMemory {
 
     @Bean
     public UserDetailsService usersMemory() {
