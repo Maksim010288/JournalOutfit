342780cc5044e0b763aa256a398ecbd1f35ca565
Maksim <mysik.bazyk@gmail.com>
2023-09-14 23:04:56 +0300

    create new class

--- src/main/java/voe/company/OutfitsCompletedOfLog/service/UserDetService.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/service/UserDetService.java
@@ -1,2 +1,39 @@
-package voe.company.OutfitsCompletedOfLog.service;public class UserDetailsService {
+package voe.company.OutfitsCompletedOfLog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import voe.company.OutfitsCompletedOfLog.entity.UsersEntity;
+import voe.company.OutfitsCompletedOfLog.repository.UserRepository;
+
+@Service
+public class UserDetService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UsersEntity users = findByUsername(username);
+        if (users == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserMagazineDetails(users);
+    }
+
+    private UsersEntity findByUsername(String userName) {
+        for (UsersEntity entity : userRepository.findAll()) {
+            if (entity.getEmail().equals(userName)) {
+                return entity;
+            }
+        }
+        return null;
+    }
 }
