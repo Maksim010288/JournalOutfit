f58e46926bd0b3f27de364ef0fb343db303ebed0
Maksim <mysik.bazyk@gmail.com>
2023-10-05 16:22:24 +0300

    added two method to search by name and by date

--- src/main/java/voe/company/OutfitsCompletedOfLog/repository/MagazineRepository.java
+++ src/main/java/voe/company/OutfitsCompletedOfLog/repository/MagazineRepository.java
@@ -1,9 +1,17 @@
 package voe.company.OutfitsCompletedOfLog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import voe.company.OutfitsCompletedOfLog.entity.JournalEntity;
 
+import java.util.List;
+
 @Repository
 public interface MagazineRepository extends JpaRepository<JournalEntity, Long> {
+    @Query(value = "select * from outfits_journal where date = ?", nativeQuery = true)
+    List<JournalEntity> findByDate(String date);
+
+    @Query(value = "select * from outfits_journal where performer = ?", nativeQuery = true)
+    List<JournalEntity> findByName(String name);
 }
